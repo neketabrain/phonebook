31959abedfaf7332a1a630c18794a5ceb94ed138
Nikita Karapuzov <karapuzov.nikita@gmail.com>
2021-04-02 16:47:43 +0700

    edit contact

--- src/views/EditContact/EditContact.tsx
+++ src/views/EditContact/EditContact.tsx
@@ -1,9 +1,41 @@
-import React, { FC } from 'react';
+import React, { useCallback, FC } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, useHistory } from 'react-router-dom';
+
+import { ContactForm, LoaderBlock, ReturnLink } from 'src/components';
+import { ContactFormValues } from 'src/components/ContactForm/ContactForm.types';
+import { PATHS } from 'src/constants';
+import { getContactById, getContactsLoading, editContact } from 'src/store/contacts';
+import { RouterIdParam } from 'src/types';
 
 import styles from './EditContact.module.scss';
 
 const EditContact: FC = () => {
-  return <div />;
+  const { id } = useParams<RouterIdParam>();
+  const history = useHistory();
+
+  const isLoading = useSelector(getContactsLoading);
+  const contact = useSelector(getContactById(Number(id)));
+  const dispatch = useDispatch();
+
+  const updateContact = useCallback(
+    (values: ContactFormValues) => {
+      if (contact?.id) {
+        dispatch(editContact(contact.id, values));
+        history.push(`${PATHS.contacts}/${contact.id}`);
+      }
+    },
+    [history, dispatch, contact]
+  );
+
+  return (
+    <div className={styles.container}>
+      {isLoading && <LoaderBlock />}
+
+      <ReturnLink className={styles.return} />
+      <ContactForm onSubmit={updateContact} defaultValues={contact} buttonLabel="Сохранить" />
+    </div>
+  );
 };
 
 export default EditContact;
